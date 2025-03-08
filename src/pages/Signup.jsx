93863623738cb92813205caf0df5073435f69ffa
vidakpop <daviskpop@gmail.com>
2025-03-08 21:08:11 +0300

    Add Signup component with basic structure

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
+const Signup = () => {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
