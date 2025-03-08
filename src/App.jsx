d4856592287596120d34aed7c574e9b7e67027c4
vidakpop <daviskpop@gmail.com>
2025-03-08 17:46:39 +0300

    Import Login component into App and update routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Routes,Route } from 'react-router-dom'
+import { Router,Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Login from './pages/Login'
 
 const App = () => {
   return (
