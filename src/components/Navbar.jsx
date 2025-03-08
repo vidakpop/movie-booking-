9fae1866c50ecf8fea2adfc78901b6aaeaf3bd8d
vidakpop <daviskpop@gmail.com>
2025-03-08 17:25:42 +0300

    Add navigation links to Navbar component for improved routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,13 @@ const Navbar = () => {
         className="fixed top-0 w-full bg-black bg-opacity-50 p-4 flex justify-between items-center z-50"
     >
         <Link to="/" className="text-neon text-2xl font-bold">🎬 MovieVerse</Link>
+        <div className="space-x-4 hidden md:flex">
+            <Link to="/" className="text-white">Home</Link>
+            <Link to="/movies" className="text-white">Movies</Link>
+            <Link to="/login" className="text-white">Login</Link>
+            <Link to="/signup" className="text-white">Sign Up</Link>
+
+        </div>
 
     </motion.nav>
   )
