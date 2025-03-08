75af3f304d92599dd44deb365292cc75f68c500d
vidakpop <daviskpop@gmail.com>
2025-03-08 18:05:33 +0300

    Add password input and signup link to Login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,6 +13,9 @@ const Login = () => {
         <div className='bg-black bg-opacity-60 p-8 rounded-lg shadow-lg'>
             <h2 className='text-3xl text-neon mb-4'>Login</h2>
             <input type="email" placeholder='Email' className='w-full p-2 my-2 bg-gray-800 border border-neon text-white' />
+            <input type="password" placeholder="Password" className="w-full p-2 my-2 bg-gray-800 border border-neon text-white" />
+            <button className="bg-neon w-full py-2 rounded mt-4">Login</button>
+            <p className="mt-4 text-gray-400">Don't have an account? <Link to="/signup" className="text-neon">Sign up</Link></p>
 
 
         </div>
