26900c58bb7d819dc411fb5c3b4086cf3901bf94
vidakpop <daviskpop@gmail.com>
2025-03-08 21:13:03 +0300

    Add Signup route to App component for user navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/login' element={<Login />}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </div>
   )
