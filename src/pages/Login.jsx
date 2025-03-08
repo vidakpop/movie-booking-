9e87793ec9c02268b470b082d94234eb417dac6f
vidakpop <daviskpop@gmail.com>
2025-03-08 18:01:56 +0300

    Enhance Login component with title and email input for better user experience

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,9 +8,12 @@ const Login = () => {
         initial={{ opacity: 0 ,scale: 0.8}}
         animate={{ opacity: 1 ,scale: 1}}
        
-        className="flex flex-col items-center justify-center h-screen"
+        className="flex  items-center justify-center h-screen"
     >
         <div className='bg-black bg-opacity-60 p-8 rounded-lg shadow-lg'>
+            <h2 className='text-3xl text-neon mb-4'>Login</h2>
+            <input type="email" placeholder='Email' className='w-full p-2 my-2 bg-gray-800 border border-neon text-white' />
+
 
         </div>
 
