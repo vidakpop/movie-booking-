b55a2e6339b36a55c42b42902b8f2fe3b4248bba
vidakpop <daviskpop@gmail.com>
2025-03-10 19:18:00 +0300

    Fix dependency array in useEffect for movies data fetching

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -9,7 +9,7 @@ const movies = () => {
         axios.get("http://127.0.0.1:8000/api/movies/")
         .then(response => setMovies(response.data))
         .catch(error => console.error("Error fetching movies", error))
-    })
+    },[])
   return (
     <div>movies</div>
   )
