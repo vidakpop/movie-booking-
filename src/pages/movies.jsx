708f421cbbe5072597b9d41d54f98183496b2442
vidakpop <daviskpop@gmail.com>
2025-03-10 19:12:29 +0300

    Add Movies component with initial structure and basic layout

---
+++ src/pages/movies.jsx
@@ -0,0 +1,11 @@
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+import { motion } from 'framer-motion'
+
+const movies = () => {
+  return (
+    <div>movies</div>
+  )
+}
+
+export default movies
