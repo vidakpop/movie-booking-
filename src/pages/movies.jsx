fbc6c243a50b354a0c05e6f866bdcb3a23dd7ff9
vidakpop <daviskpop@gmail.com>
2025-03-10 19:17:29 +0300

    Fetch and handle movies data from API in Movies component

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -7,6 +7,8 @@ const movies = () => {
 
     useEffect(()=>{
         axios.get("http://127.0.0.1:8000/api/movies/")
+        .then(response => setMovies(response.data))
+        .catch(error => console.error("Error fetching movies", error))
     })
   return (
     <div>movies</div>
