66310ba71e9065d185ecf0fe3137bd8c7cb5925d
vidakpop <daviskpop@gmail.com>
2025-03-13 10:20:53 +0300

    Add Feature component with initial structure and imports

---
+++ src/components/Feature.jsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+import { section } from "framer-motion/client";
+import { FaFilm, FaTheaterMasks, FaMobileAlt, FaRegCreditCard } from "react-icons/fa";
+
+const Feature = () => {
+  return (
+    <section>
+        
+    </section>
+  )
+}
+
+export default Feature
