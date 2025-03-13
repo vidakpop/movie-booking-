a5429867ebefb5972d50f4c9a8ad0aa52dbf2267
vidakpop <daviskpop@gmail.com>
2025-03-13 09:50:56 +0300

    Add Hero component to Dashboard for enhanced visual presentation

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 const Dashboard = () => {
   return (
     <div className='relative  w-full h-screen overflow-hidden bg-black text-white'>
-     
+      <Hero />
     </div>
   )
 }
