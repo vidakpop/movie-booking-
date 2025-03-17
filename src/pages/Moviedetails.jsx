98ed761f2a79eaceff1b6d79b5b971fb4e45c4db
vidakpop <daviskpop@gmail.com>
2025-03-17 17:26:28 +0300

    Add imports for routing, axios, and animation in Moviedetails component

--- src/pages/Moviedetails.jsx
+++ src/pages/Moviedetails.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useParams,useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { motion } from 'framer-motion'
 
 const Moviedetails = () => {
   return (
