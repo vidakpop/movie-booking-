f0eb5f94936b3e9e28b27bd1a98dfb5d75973ed6
vidakpop <daviskpop@gmail.com>
2025-03-17 17:30:55 +0300

    Fetch movie details using axios in Moviedetails component

--- src/pages/Moviedetails.jsx
+++ src/pages/Moviedetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams,useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { motion } from 'framer-motion'
@@ -8,7 +8,16 @@ const Moviedetails = () => {
     const navigate = useNavigate()
     const [movie, setMovie] = useState(null)
     const [loading, setLoading] = useState(true)
-    
+
+    useEffect(() => {
+        axios
+          .get(`http://127.0.0.1:8000/api/movies/${id}/`)
+          .then((response) => {
+            setMovie(response.data);
+            setLoading(false);
+          })
+          .catch((error) => console.error("Error fetching movie details", error));
+      }, [id]);
   return (
     <div>Moviedetails</div>
   )
