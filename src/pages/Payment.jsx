81ed0d0fdf3f7b3ef3d7b57e5daa5c9ec4598e59
vidakpop <daviskpop@gmail.com>
2025-04-03 21:21:44 +0300

    Add Payment component with basic structure

---
+++ src/pages/Payment.jsx
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from 'react'
+import { useLocation,useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { motion } from 'framer-motion'
+
+const Payment = () => {
+  return (
+    <div>Payment</div>
+  )
+}
+
+export default Payment
